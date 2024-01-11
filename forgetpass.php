cc10af950699c9694617efc98740b605a87f08ef
DeepMathukiya <dsmathukiya2004@gmail.com>
2024-01-11 19:28:02 +0530

    Forget Password

--- forgetpass.php
+++ forgetpass.php
@@ -7,7 +7,27 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <style>
+        *{
+            background-color: #f8f9fa;
+        }
+        .form_input{
+            display: flex;
+            justify-content: center;
+            margin:20px;
+        }
+        .form-outline input{
+            width: 400px;
+            max-width:500px;
+        }
+        .text-center{
+            margin:0 40%;
+        }
+    </style>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  
   </head>
   <body>
       
@@ -16,13 +36,19 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <form action="" method="post">
-<div class="form-group">
-  <label for=""></label>
-  <input type="email" class="form-control" name="email" id="" aria-describedby="emailHelpId" placeholder="">
-  <small id="emailHelpId" class="form-text text-muted">email</small>
-</div>
-    <button type="submit" class="btn btn-primary" name ="submit">Send mail</button>
+    
+    <form method="POST" id="" action="">
+        <div class="form_input">
+            <!-- <i class="fas fa-key fa-lg me-3 fa-fw"></i> -->
+            <div class="form-outline  mt-5">
+                <label class="form-label text-center" for="OTPverify">
+                   Enter your Email</label>
+                <input type="email" name="email"  class="form-control"/>
+            </div>
+        </div>
+        <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4 ">
+            <input type="submit" value="Send mail" id="insert" class="btn btn-primary btn-lg" name="submit">
+        </div>
     </form>
 
 
@@ -65,7 +91,7 @@ try {
     //Content
     $mail->isHTML(true);                                  //Set email format to HTML
     $mail->Subject = "Reset Passwordlink";
-    $mail->Body    = "Please reffer to below link <a href='resetpsw.php?pass=$pass'>link</a> ";
+    $mail->Body    = "Please reffer to below link <a href='http://localhost/medecio/resetpsw.php?pass=$pass'>link</a> ";
 
     $mail->send();
 
--- resetpsw.php
+++ resetpsw.php
@@ -7,49 +7,70 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <style>
+        *{
+            background-color: #f8f9fa;
+        }
+        .form_input{
+            display: flex;
+            justify-content: center;
+            margin:20px;
+        }
+        .form-outline input{
+            width: 400px;
+            max-width:500px;
+        }
+        .text-center{
+            margin:0 40%;
+        }
+    </style>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+ 
   </head>
+
   <body>
       
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <form action="" method="post">
-        <div class="form-group">
-          <input type="text" name="pass" id="" class="form-control" placeholder="new password" aria-describedby="helpId">
+      <div class="form_input">
+        <div class="form-outline  mt-5">
+          <input type="password" name="pass" id="" class="form-control" placeholder="new password" aria-describedby="helpId">
+        </div></div>
+        <div class="form_input">
+
+        <div class="form-outline  mt-5">
+          <input type="password" name="conpass" id="" class="form-control" placeholder="new password" aria-describedby="helpId">
+        </div></div>
+        <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4 ">
+        <input type="submit"class="btn btn-primary btn-lg" value="ResetPassword" name = "submit">
         </div>
-        <div class="form-group">
-          <input type="text" name="conpass" id="" class="form-control" placeholder="new password" aria-describedby="helpId">
-        </div>
-        <input type="submit" value="ResetPassword" name = "submit">
+      </div>
     </form>   
 
 <?php
 include "connection.php";
 $pass1 = $_GET['pass'];
-if ($pass1 =null){
-?>
-<script>
-  alert("don`t try to oversmart");
-    location.replace("/");
-</script>
+  
 
-<?php
-}
-else{
 if(isset($_POST['submit'])){
   $pass = $_POST['pass'];
   $conpass = $_POST['conpass'];
-  $que = "select * from registration where pass = $pass1";
+  $pass2 = mysqli_real_escape_string($con, $_POST['pass']);
+  $cpass = password_hash($pass2, PASSWORD_BCRYPT);
+  $que = "select * from registration where pass = '$pass1'";
   $res = mysqli_query($con , $que);
   $noOfRow = mysqli_num_rows($res);
   if($noOfRow){
     if($pass = $conpass){
-    $que1 = "update registration set pass = $pass where pass = $pass1";
+    $que1 = "update registration set pass = '$cpass' where pass='$pass1'";
     $res2  = mysqli_query($con , $que1);
-    }
+  }
     else{
 ?>
-<script>alert();</script>
+<script>alert("confirm password should be matched");</script>
 <?php
     }
   }
@@ -66,7 +87,7 @@ if(isset($_POST['submit'])){
 
 
 
-}
+
 ?>
 </body>
 </html>
